d9c5f7b3c93a1d99fd212703fe2c258d41fad1c1
sprout42 <a.aaron.cornelius@gmail.com>
2022-07-08 12:39:49 -0400

    fixed up placeholder eTPU2 peripheral

--- cm2350/peripherals/etpu2.py
+++ cm2350/peripherals/etpu2.py
@@ -3,17 +3,17 @@ import enum
 from ..ppc_vstructs import *
 from ..ppc_peripherals import *
 
-import envi.memory as e_mem
+#import envi.memory as e_mem
 
 import logging
 logger = logging.getLogger(__name__)
 
 __all__ = [
-    'ETPU2Device',
-    'ETPU2',
+    #'ETPU2Device',
+    'eTPU2',
 ]
 
-
+"""
 class ETPU2Device(enum.Enum):
     Registers       = enum.auto()
     ParamRAM        = enum.auto()
@@ -27,20 +27,33 @@ ETPU2_DEVICE_MMIO_SIZE = {
     ETPU2Device.ParamRAMMirror: 0x00001800,
     ETPU2Device.CodeRAM:        0x00006000,
 }
+"""
 
 
-class ETPU2(MMIOPeripheral):
-    '''
-    This is the FMPLL Controller.
-    '''
+class eTPU2(MMIOPeripheral):
     def __init__(self, emu, mmio_addr):
-        # TODO: implement eTPU2 registers and connetions to the rest of the
+        # TODO: implement eTPU2 registers and connections to the rest of the
         # MPC5674F.  For now the read/write functions are implemented with
         # placeholder functions that return 0's and discard writes.
-        super().__init__(emu, 'ETPU2', mmio_addr, ETPU2_DEVICE_MMIO_SIZE[ETPU2Device.Registers])
+        #
+        # The eTPU peripheral has a few separate MMIO regions:
+        #
+        #           Function          |       Memory Range
+        #   --------------------------+------------------------
+        #   eTPU Registers            | 0xC3FC_0000—0xC3FC_3FFF
+        #   (Reserved)                | 0xC3FC_4000—0xC3FC_7FFF
+        #   eTPU Parameter RAM        | 0xC3FC_8000—0xC3FC_97FF
+        #   eTPU Parameter RAM Mirror | 0xC3FC_C000—0xC3FC_D7FF
+        #   eTPU Code RAM             | 0xC3FD_0000—0xC3FD_5FFF
+        #   (Reserved)                | 0xC3FD_6000—0xC3FE_FFFF
+        #
+        # Instead of having seperate MMIO regions, use one giant memory region.
+        super().__init__(emu, 'eTPU', mmio_addr, 0x30000)
 
-        self.param_ram = bytearray(ETPU2_DEVICE_MMIO_SIZE[ETPU2Device.ParamRAM])
-        self.code_ram  = bytearray(ETPU2_DEVICE_MMIO_SIZE[ETPU2Device.CodeRAM])
+        # TODO: figure out the param/code ram regions should work, valid access
+        # range can be indicated with SCMSIZE somehow?
+        #self.param_ram = bytearray(ETPU2_DEVICE_MMIO_SIZE[ETPU2Device.ParamRAM])
+        #self.code_ram  = bytearray(ETPU2_DEVICE_MMIO_SIZE[ETPU2Device.CodeRAM])
 
     def _getPeriphReg(self, offset, size):
         # return placeholder data
@@ -50,6 +63,7 @@ class ETPU2(MMIOPeripheral):
         # Do nothing
         pass
 
+    """
     def _param_ram_read(self, va, offset, size):
         value = self.param_ram[offset:offset+size]
         logger.debug("0x%x:  eTPU2 ParamRAM read  [%x:%r] (%s)", self.emu.getProgramCounter(), va, size, value.hex())
@@ -118,4 +132,4 @@ class ETPU2(MMIOPeripheral):
             raise Exception('Invalid device type set MMIO address of core %s registers with setAddr()' % self.__class__.__name__)
 
         emu.addMMIO(**args)
-
+    """

19be5e1abbf77035a57931b9d56f343d7c638b42
Erin <493589+sprout42@users.noreply.github.com>
2023-11-13 10:35:22 -0500

    fixes during testing

--- scripts/keplers_law_test.py
+++ scripts/keplers_law_test.py
@@ -35,90 +35,28 @@ def print_msg(msg, client_name=None):
     print('%s %-12s %08x  [%d]  %s' % (ts, client_name, msg.arbid, msg.length, msg.data.hex(' ', 1)))
 
 
-
-#def run_test(client, client_name, msgq, arbid, data, resp_mask, resp_arbid, resp_len):
 def run_test(client, client_name, msgq, arbid, data):
     msg = CanMsg(rtr=0, ide=1, arbid=arbid, length=len(data), data=data)
-
-    # Before sending clear out the list received msgs
-    try:
-        while True:
-            msgq.get_nowait()
-    except queue.Empty:
-        pass
-
     print_msg(msg, '%s ->' % client_name)
     client.send(msg)
 
-    #time.sleep(240)
 
-    #try:
-    #    while True:
-    #        name, resp = msgq.get_nowait()
-    #        if name == client_name and resp.arbid & mask == resp_arbid and \
-    #                resp.length == resp_len:
-    #            return resp
-    #except queue.Empty:
-    #    pass
-    #return None
-
-
-def test_lower_input_boundary(client, client_name, msgq):
+def test_lower_input_boundary(client, client_name):
     msg = CanMsg(rtr=0, ide=1, arbid=TEST_ARBID, length=8, data=b'\x00'*8)
     print_msg(msg, '%s ->' % client_name)
     client.send(msg)
 
 
-def test_upper_input_boundary(client, client_name, msgq):
+def test_upper_input_boundary(client, client_name):
     msg = CanMsg(rtr=0, ide=1, arbid=TEST_ARBID, length=8, data=b'\xFF'*8)
     print_msg(msg, '%s ->' % client_name)
     client.send(msg)
 
 
-def test(client, client_name, msgq):
+def test(client, client_name):
     msg = CanMsg(rtr=0, ide=1, arbid=TEST_ARBID, length=8, data=b'\x80\x31\xD4\x1F\xD8\x00\x00\x00')
     print_msg(msg, '%s ->' % client_name)
     client.send(msg)
-
-
-def test_converged_inputs(client, client_name, msgq):
-    convergence_failure = 0
-    N = 2
-    for i in range(1, N + 1):
-        angle = 360 * i / N
-        ecc = .338
-
-        encoded_angle = int((angle + 210) * 1000000)
-        encoded_ecc = int(ecc / 0.0015625)
-        msg_data = struct.pack('<LHH', encoded_angle, encoded_ecc, 0xFFFF)
-
-        resp = run_test(client, client_name, msgq, TEST_ARBID, msg_data,
-                RESP_MASK, RESP_ARBID, 8)
-
-        if resp is None:
-            convergence_failure += 1
-        else:
-            # If a message was received check the results
-            encoded_ecc_anom_degrees = struct.unpack_from('<L', resp, 0)[0]
-            ecc_anom_degrees = encoded_ecc_anom_degrees * 0.000001 - 210
-            if ecc_anom_degrees >= -210 and ecc_anom_degrees <= 210:
-                print('Response within range: -210 <= %f <= 210' % ecc_anom_degrees)
-            else:
-                print('Response out of range: %f' % ecc_anom_degrees)
-                convergence_failure += 1
-
-    return convergence_failure
-
-
-def test_for_vulnerable_inputs_unpatched(client, client_name, msgq):
-    convergence_failure = test_converged_inputs(client, client_name, msgq)
-    # For the vulnerable version we expect there to be convergence failures
-    assert convergence_failure > 0
-
-
-def test_for_vulnerable_inputs_patched(client, client_name, msgq):
-    convergence_failure = test_converged_inputs(client, client_name, msgq)
-    assert convergence_failure == 0
 
 
 def recv_thread(client, name, msgq):
@@ -127,6 +65,15 @@ def recv_thread(client, name, msgq):
         if msg is None:
             break
         print_msg(msg, name)
+
+        if msg.arbid & RESP_MASK == RESP_ARBID:
+            encoded_ecc_anom_degrees = struct.unpack_from('<L', msg.data, 0)[0]
+            ecc_anom_degrees = encoded_ecc_anom_degrees * 0.000001 - 210
+            if ecc_anom_degrees >= -210 and ecc_anom_degrees <= 210:
+                print('Response within range: -210 <= %f <= 210' % ecc_anom_degrees)
+            else:
+                print('Response out of range: %f' % ecc_anom_degrees)
+
         msgq.put((client._addr, msg))
 
 
@@ -151,22 +98,22 @@ def run(*ports):
     #clients[0].send(msg)
     #time.sleep(1)
 
-    #msg = CanMsg(rtr=0, ide=1, arbid=0x18eaffb6, length=3, data=b'\xeb\xfe\xb6')
-    #print_msg(msg, '%s ->' % thread_names[0])
-    #clients[0].send(msg)
+    input()
+    msg_data = bytes.fromhex('8031D41F8000FFFF')
+    run_test(clients[0], thread_names[0], msgq, TEST_ARBID, msg_data)
 
-    #input()
-    #test_lower_input_boundary(clients[0], thread_names[0], msgq)
-
-    #input()
-    #test_upper_input_boundary(clients[0], thread_names[0], msgq)
-
+    i = 0
+    N = 10
     while True:
         input()
-        test_upper_input_boundary(clients[0], thread_names[0], msgq)
-        #test_for_vulnerable_inputs_unpatched(clients[0], thread_names[0], msgq)
-        #test_for_vulnerable_inputs_patched(clients[0], thread_names[0], msgq)
-        #test(clients[0], thread_names[0], msgq)
+        angle = 360 * i / N
+        ecc = .338
+        encoded_angle = int((angle + 210) * 1000000)
+        encoded_ecc = int(ecc / 0.0015625)
+        print('Sending: angle %f (%d), ecc %f (%d)' % (angle, encoded_angle, ecc, encoded_ecc))
+        msg_data = struct.pack('<LHH', encoded_angle, encoded_ecc, 0xFFFF)
+        run_test(clients[0], thread_names[0], msgq, TEST_ARBID, msg_data)
+        i += 1
 
 
 def main():
